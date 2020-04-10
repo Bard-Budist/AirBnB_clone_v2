c6efab013e7bf2373ac3df9bebc56f03264546a3
Bard-Budist <op7puro@live.com>
2020-04-09 20:15:32 -0500

    Fix task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,6 +22,11 @@ def do_deploy(archive_path):
         run("rm /tmp/" + name + ".tgz")
         run("rm -rf /data/web_static/current")
         run(
+            "mv /data/web_static/releases/"
+            + name
+            + "/web_static/* /data/web_static/releases/"
+            + name + "/")
+        run(
             "ln -s /data/web_static/releases/"
             + name + " /data/web_static/current")
         print("New version deployed!")
