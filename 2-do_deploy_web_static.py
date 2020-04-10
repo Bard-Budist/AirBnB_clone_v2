755058b8e9750505197bdd1d0a52a026dba9695d
Bard-Budist <op7puro@live.com>
2020-04-10 17:46:53 -0500

    Fix task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""dawdawd"""
 from fabric.api import *
 import os.path
 
@@ -14,19 +15,23 @@ def do_deploy(archive_path):
         nameFile = archive_path.split('/')[1]
         put(archive_path, "/tmp/")
         name = os.path.splitext(nameFile)[0]
-        run("mkdir -p /data/web_static/releases/" + name)
-        run(
+        sudo("mkdir -p /data/web_static/releases/" + name)
+        sudo(
             "tar -xzf /tmp/" +
             name + ".tgz -C /data/web_static/releases/"
             + name)
-        run("rm /tmp/" + name + ".tgz")
-        run("rm -rf /data/web_static/current")
-        run(
-            "mv /data/web_static/releases/"
+        sudo("rm /tmp/" + name + ".tgz")
+        sudo(
+            "mv -f /data/web_static/releases/"
             + name
             + "/web_static/* /data/web_static/releases/"
-            + name + "/")
-        run(
+            + name)
+        sudo(
+            "rm -rf /data/web_static/releases/"
+            + name
+            + "/web_static")
+        sudo("rm -rf /data/web_static/current")
+        sudo(
             "ln -s /data/web_static/releases/"
             + name + " /data/web_static/current")
         print("New version deployed!")
