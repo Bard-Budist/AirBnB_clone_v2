da13f3cce48ce53e9daafdd84a4e99bcad00c110
Bard-Budist <op7puro@live.com>
2020-04-22 14:28:13 +0000

    Fix task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,20 +3,24 @@
 from flask import Flask
 from flask import render_template
 from models import storage
-
-
+from models.state import State
 
 app = Flask(__name__)
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
+
 @app.teardown_appcontext
-def close():
+def close(error):
     storage.close()
+
 
 @app.route("/states_list", strict_slashes=False)
 def task7():
-    return render_template("7-states_list.html")
+    return render_template(
+            "7-states_list.html",
+            state=storage.all(State),
+            name_class=State.__name__)
 
 
 if __name__ == "__main__":
